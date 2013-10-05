2440b9349a25ad3f0fb8da20dc2ff2b671515399
Kedar <kedardeore@gmail.com>
2013-10-05 21:57:01 +0530

    misc cleanup

--- js/popup.js
+++ js/popup.js
@@ -12,20 +12,20 @@ function clear_code(){
 
 $(function(){
 	// Check language item last selected:
-	var $storage_lang, $storage_style;
-	var $code, $lang, $style, $divstyles;
+	var stored_lang, stored_style;
+	var code, lang, style, $divstyles;
 
 	chrome.storage.sync.get(null, function(data){
 		if (data['lang']){
-			$storage_lang = data['lang'];
-			if($('#lang-dropdown').val() != $storage_lang){
-				$('#lang-dropdown').val($storage_lang);
+			stored_lang = data['lang'];
+			if($('#lang-dropdown').val() != stored_lang){
+				$('#lang-dropdown').val(stored_lang);
 			}
 		}
 		if(data['style']){
-			$storage_style = data['style'];
-			if($('#style-dropdown').val() != $storage_style){
-				$('#style-dropdown').val($storage_style);
+			stored_style = data['style'];
+			if($('#style-dropdown').val() != stored_style){
+				$('#style-dropdown').val(stored_style);
 			}
 		}
 		if (data['rounded-corners']){
@@ -38,8 +38,8 @@ $(function(){
 
 	$('div.options').on('change', function(){
 		var border_radius, borders;
-		$lang = $('#lang-dropdown').val();
-		$style = $('#style-dropdown').val();
+		lang = $('#lang-dropdown').val();
+		style = $('#style-dropdown').val();
 		$('input[type=checkbox]:checked').each(function(){
 			if (this.id == 'rounded-corners'){
 				border_radius = 'checked';
@@ -56,14 +56,14 @@ $(function(){
 				borders = '';
 			}
 		});
-			chrome.storage.sync.set({
-				'lang':$lang, 
-				'style':$style, 
-				'rounded-corners':border_radius,
-				'borders':borders
-			}, function(){
-				console.log('lang:'+$lang+' style:'+$style+' rc:'+border_radius+' border:'+borders);
-			});
+		chrome.storage.sync.set({
+			'lang':lang, 
+			'style':style, 
+			'rounded-corners':border_radius,
+			'borders':borders
+		}, function(){
+			console.log('[+] chrome.storage.sync.set');
+		});
 	});
 
 	$('#clip').on('click', function(){
@@ -75,10 +75,10 @@ $(function(){
 
 	$('#clear').on('click', clear_code);
 
-	$('#hl').on('click', function(){
-		$code = $('#code_area').val();
-		$lang = $('#lang-dropdown').val();
-		$style = $('#style-dropdown').val();
+	$('#hglt').on('click', function(){
+		code = $('#code_area').val();
+		lang = $('#lang-dropdown').val();
+		style = $('#style-dropdown').val();
 		$divstyles = 'padding:.2em .6em;';
 		$('input[type=checkbox]:checked').each(function(){
 			if (this.id == 'rounded-corners'){
@@ -88,7 +88,7 @@ $(function(){
 				$divstyles += 'border:solid gray;border-width:.1em .1em .1em .8em;';
 			}
 		});
-		hilite.getCode($code,$lang,$style,$divstyles);
+		hilite.getCode(code,lang,style,$divstyles);
 	});
 
 });
